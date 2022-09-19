0659b636cb419d5cd6036141083378688581dd69
Jan <58750565+ThatJanGuy@users.noreply.github.com>
2022-09-19 16:31:54 +0200

    All but colorful output.

--- AssetTracking/Program.cs
+++ AssetTracking/Program.cs
@@ -1,7 +1,33 @@
 ﻿using AssetTracking;
 using LittleHelpers;
+using System.Globalization;
 
+CurrencyConverter currencyConverter = new CurrencyConverter();
+AssetList assetList = new AssetList();
+CultureInfo culture = CultureInfo.InvariantCulture;
 
-AssetList assetList = new();
+// Add a bunch of test data to make function checking simpler.
+Console.Clear();
+while (true)
+{
+    Console.WriteLine("Would you like to add some test data? (y/n)");
+    char input = Console.ReadKey().KeyChar;
+
+    if ( input != 'y' && input != 'n')
+    {
+        TextManipulation.ColoredText(
+            "Invalid input. Only 'y' or 'n' allowed.",
+            "Red"
+            );
+        continue;
+    }
+
+    if (input == 'n') break;
+
+    assetList.AddTestData();
+    break;
+}
 
 assetList.AddAsset();
+
+assetList.Display();
