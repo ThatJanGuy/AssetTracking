ecbd474bda5f294e1029add52c94604226f1536f
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2022-10-18 19:12:01 +0200

    Introduce a Main Menu (wip).

--- AssetTracking/Program.cs
+++ AssetTracking/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AssetTracking;
 using LittleHelpers;
 using System.Globalization;
+using System.Text.Json;
 using static LittleHelpers.GetInput;
 using static LittleHelpers.TextManipulation;
 
@@ -30,6 +31,66 @@ while (true)
     break;
 }
 
-assetList.AddAsset();
+while (true)
+{
+    MakeHeading("Main Menu", '=');
+    Console.Write(
+                $"(1) to ADD an asset.\n" +
+                $"(2) to EDIT an asset.\n" +
+                $"(3) to DELETE an asset.\n" +
+                $"(4) for STATISTICS.\n" +
+                $"(x) to SAFE AND EXIT.\n" +
+                $"Enter choice > ");
 
-assetList.Display();
+    int menuChoice = Console.ReadKey().KeyChar;
+
+    bool cancel = false;
+
+    switch (menuChoice)
+    {
+        case '1':
+            assetList.AddAsset();
+            break;
+
+        case '2':
+            int? idToEdit = null;
+            while (idToEdit == null)
+            {
+                Console.Write("\nEnter the ID of the asset you wish to edit ('c' to cancel) > ");
+                idToEdit = GetInt(out cancel, "c", 0, orderedList.Count - 1);
+                if (cancel) break;
+            }
+            if (cancel) assetList.Display();
+
+            Guid targetTransactionGuid = orderedList[idToEdit.Value].Id;
+
+            Edit(targetTransactionGuid);
+            break;
+            
+        case '3':
+            int? idToDelete = null;
+            while (idToDelete == null)
+            {
+                Console.Write("\nEnter the ID of the asset you wish to delete ('c' to cancel) > ");
+                idToDelete = GetInt(out cancel, "c", 0, orderedList.Count - 1);
+                if (cancel) break;
+            }
+            if (cancel) Display();
+
+            DeleteTransaction(orderedList[(int)idToDelete]);
+            break;
+
+        case '4':
+
+            break;
+        case 'x':
+
+            break;
+        default:
+            ColoredText("\nPlease enter a valid option.\n(Press any key to continue)\n", ConsoleColor.Red);
+            Console.ReadKey();
+            assetList.Display();
+            break;
+    }
+}
+
